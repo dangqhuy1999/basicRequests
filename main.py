df62dc5743e3d333fddc952cc0637d7b2040aa43
dangqhuy1999 <90665005+dangqhuy1999@users.noreply.github.com>
2025-04-01 09:46:40 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+headers = {
+    'x-api-key': ''
+}
+
+response = requests.get('https://intranet.cennext.com/api/discord_subscribe', headers=headers)
+
+print("Status Code:", response.status_code)
+print("Status Code:", response.text)
+# Chuyển response text thành JSON nếu có thể
+try:
+    response_json = response.json()  # Chuyển response thành dictionary
+    formatted_json = json.dumps(response_json, indent=4, ensure_ascii=False)
+    print("Response JSON:\n", formatted_json)
+except json.JSONDecodeError:
+    print("Response Text:", response.text)  # Nếu không phải JSON, in text bình thường
